cc8b85726de3898b4783101d6325d293e63d8154
zhuwenwen <zhuwenwen@tuhu.cn>
2018-11-11 19:20:41 +0800

    添加博客修改

--- src/main/java/cn/edu/aufe/cstfirst/controller/BlogController.java
+++ src/main/java/cn/edu/aufe/cstfirst/controller/BlogController.java
@@ -4,11 +4,15 @@ package cn.edu.aufe.cstfirst.controller;
 import cn.edu.aufe.cstfirst.common.annotation.AuthenticationParam;
 import cn.edu.aufe.cstfirst.common.annotation.SkipLogon;
 import cn.edu.aufe.cstfirst.domian.Blog;
+import cn.edu.aufe.cstfirst.handler.BlogException;
 import cn.edu.aufe.cstfirst.handler.Result;
 import cn.edu.aufe.cstfirst.service.BlogService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
 
 /**
  * @author zhuwenwen
@@ -33,6 +37,18 @@ public class BlogController {
      */
     @PostMapping("save")
     public Result add(@RequestBody Blog blog, @AuthenticationParam String username) {
+        if (StringUtils.isBlank(blog.getTitle())){
+            throw new BlogException("博客标题不能为空",-1);
+        }
+        if (StringUtils.isBlank(blog.getTags())){
+            throw new BlogException("请添加博客标签",-1);
+        }
+        if (StringUtils.isBlank(blog.getContent())){
+            throw new BlogException("请填写博客内容",-1);
+        }
+        blog.setCrateTime(LocalDateTime.now());
+        blog.setState(1);
+        blog.setUpdateTime(LocalDateTime.now());
         return Result.success(blogService.saveBlog(blog, username));
     }
 
